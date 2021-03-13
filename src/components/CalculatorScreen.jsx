d0e53a21ef051c82e4ea35cb589bf6be490c9a6d
Helio <heliosoto17@gmail.com>
2021-03-13 20:29:51 +0100

    -reapir button line 67

--- src/components/CalculatorScreen.jsx
+++ src/components/CalculatorScreen.jsx
@@ -64,7 +64,7 @@ export const CalculatorScreen = () => {
             />
           ))}
           <CalculatorButtons
-            handleButtonPress={() => setInputState(0)}
+            handleButtonPress={() => setInputState("")}
             value={"C"}
           />
           <CalculatorButtons
